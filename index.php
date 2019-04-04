3ba466ee8fc526e438002652fb7011b8c957a272
chapabr <gui_2512@hotmail.com>
2019-04-04 18:28:46 -0300

    Origin Master

---
+++ index.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>Unlucky</title>
+	<!-- Bootstrap -->
+	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<!-- Owl Carosel -->
+	<link rel="stylesheet" type="text/css" href="libs/owlcarousel/css/owl.theme.default.min.css">
+	<link rel="stylesheet" type="text/css" href="libs/owlcarousel/css/owl.carousel.min.css">
+	<!-- Font Aawesome -->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+	<!-- Main Css -->
+	<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg fixed-top">
+  <a class="navbar-brand" href="#">Logo</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Quem Somos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Nosso Trabalho</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Membros</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contato</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<section id="home">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<h1 id="title">Unlucky</h1>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section id="quem-somos">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<h2 class="title">Quem Somos?</h2>
+				<p class="text-center">Unlucky Unlucky Unlucky Unlucky Unlucky Unlucky Unlucky Unlucky Unlucky Unlucky Unlucky Unlucky Unlucky Unlucky Unlucky Unlucky Unlucky Unlucky Unlucky Unlucky Unlucky Unlucky Unlucky Unlucky</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-4">
+				<div class="card">
+					<div class="card-body">
+						<div class="card-title text-center">
+							<h3><i class="fas fa-play"></i></h3>
+						</div>
+						<div class="card-text">
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et mi consequat, pulvinar orci nec, mattis lacus. Nulla in quam tempor, ultrices arcu in, efficitur turpis. <small>Ler Mais..</small>
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4">
+				<div class="card">
+					<div class="card-body">
+						<div class="card-title text-center">
+							<h3><i class="fas fa-play"></i></h3>
+						</div>
+						<div class="card-text">
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et mi consequat, pulvinar orci nec, mattis lacus. Nulla in quam tempor, ultrices arcu in, efficitur turpis. <small>Ler Mais..</small>
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4">
+				<div class="card">
+					<div class="card-body">
+						<div class="card-title text-center">
+							<h3><i class="fas fa-play"></i></h3>
+						</div>
+						<div class="card-text">
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et mi consequat, pulvinar orci nec, mattis lacus. Nulla in quam tempor, ultrices arcu in, efficitur turpis. <small>Ler Mais..</small>
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="galeria">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<h2 class="title">Galeria</h2>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-6">
+				<div>
+					
+				</div>
+			</div>
+			<div class="col-lg-6">
+				
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- Java Script -->
+<!-- Jquery -->
+<script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
+<!-- Bootstrap -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" integrity="sha384-xrRywqdh3PHs8keKZN+8zzc5TX0GRTLCcmivcbNJWm2rs5C8PRhcEn3czEjhAO9o" crossorigin="anonymous"></script>
+<!-- Owl Carosel -->
+<script type="text/javascript" src="libs/owlcarousel/js/owl.carousel.min.js"></script>
+<!-- Main Js -->
+<script type="text/javascript" src="assets/js/main.js"></script>
+</body>
+</html>
